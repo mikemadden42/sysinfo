bcbe01431535bf49e5fbba69dc1e9bc69400ec6e
Michael Madden <mikemadden42@icloud.com>
2015-03-10 22:38:32 -0500

    Added OS arch & type

--- sysinfo.go
+++ sysinfo.go
@@ -7,7 +7,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Temp dir:", os.TempDir())
 	fmt.Println("CPUs:", runtime.NumCPU())
 	fmt.Println("Go version:", runtime.Version())
+	fmt.Println("OS arch:", runtime.GOARCH)
+	fmt.Println("OS type:", runtime.GOOS)
+	fmt.Println("Temp dir:", os.TempDir())
 }
