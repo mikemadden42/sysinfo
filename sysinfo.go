4daa224e2bee44837b811dc20731ce1d0f23680a
Michael Madden <mikemadden42@icloud.com>
2015-09-21 15:01:22 -0500

    Updated info for cpu, host, mem

--- sysinfo.go
+++ sysinfo.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/mem"
 	"net"
 	"os"
 	"runtime"
@@ -9,18 +12,35 @@ import (
 
 func main() {
 	cpu_info()
-	os_info()
+	mem_info()
+	host_info()
 	interface_info()
 	go_info()
 }
 
 func cpu_info() {
 	fmt.Println("CPUs:", runtime.NumCPU())
+	c, _ := cpu.CPUInfo()
+	for index, each := range c {
+		fmt.Printf("CPU %d speed: %f Mhz, Cache: %d\n", index, each.Mhz, each.CacheSize)
+	}
 }
 
-func os_info() {
+func mem_info() {
+	v, _ := mem.VirtualMemory()
+	fmt.Printf("Total Memory: %v, Free: %v, UsedPercent: %f%%\n", v.Total, v.Free, v.UsedPercent)
+
+	s, _ := mem.SwapMemory()
+	fmt.Printf("Total Swap: %v, Free: %v, UsedPercent: %f%%\n", s.Total, s.Free, s.UsedPercent)
+}
+
+func host_info() {
+	h, _ := host.HostInfo()
+	fmt.Printf("Hostname: %s\n", h.Hostname)
 	fmt.Println("OS arch:", runtime.GOARCH)
-	fmt.Println("OS type:", runtime.GOOS)
+	fmt.Printf("OS: %s\n", h.OS)
+	fmt.Printf("Platform Family: %s\n", h.PlatformFamily)
+	fmt.Printf("Platform: %s %s\n", h.Platform, h.PlatformVersion)
 	fmt.Println("Temp dir:", os.TempDir())
 }
 
